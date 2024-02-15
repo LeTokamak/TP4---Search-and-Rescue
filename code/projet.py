e22f4b7b58aae435c75461cb7f647b2f8764abc3
FEBZ <kevin.gazeau@isen-ouest.yncrea.fr>
2024-02-15 12:00:45 +0100

    create dragster

--- code/projet.py
+++ code/projet.py
@@ -1,5 +1,10 @@
 from projet_import import Rover, Speeder, Balloon, Climber, run_single_server
 
+class Dragster(Speeder):
+    def _init_(self, x, y, model, environment):
+        super().__init__(self, x, y, model, environment)
+
+
 if __name__ == "__main__":
     team = []
     run_single_server()
