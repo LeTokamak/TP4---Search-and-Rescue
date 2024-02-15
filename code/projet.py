7be769a734f35f44fece78dd15ed8cf7bb969eb7
FEBZ <kevin.gazeau@isen-ouest.yncrea.fr>
2024-02-15 15:38:39 +0100

    deplacement recherche ballon et climber

--- code/projet.py
+++ code/projet.py
@@ -1,20 +1,44 @@
 from projet_import import Rover, Speeder, Balloon, Climber, run_single_server
+import random
 
 class Dragster(Speeder):
-    def _init_(self, x, y, model, environment):
-        super().__init__(self, x, y, model, environment)
+    def __init__(self, x, y, model, environment):
+        super().__init__(x, y, model, environment)
+        self.destination = [random.randint(0,100)/100,random.randint(0,100)/100]
 
     def step(self):
-        self.goto(0.1,0.1)
+        self.goto(self.destination[0],self.destination[1])
+        if self.x == self.destination[0] and self.y == self.destination[1] :
+            self.destination = [random.randint(0,100)/100,random.randint(0,100)/100]
 
-class drone_reco(Balloon):
-    def _init_(self, x, y, model, environment):
-        super().__init__(self, x, y, model, environment)
+class Antenne(Balloon):
+    def __init__(self, x, y, model, environment):
+        super().__init__(x, y, model, environment)
+        self.trajectoire = [[0.325,0.325],[0.325,1-0.325],[1-0.325,1-0.325],[1-0.325,0.325]]
+        self.step_traj = 0
     
     def step(self):
-        self.goto(0.1,0.1)
+        self.goto(self.trajectoire[self.step_traj][0],self.trajectoire[self.step_traj][1])
+        if self.x == self.trajectoire[self.step_traj][0] and self.y == self.trajectoire[self.step_traj][1] :
+            self.step_traj += 1
+            if self.step_traj > 3:
+                self.step_traj = 0
+
+class Drone_reco(Climber) :
+    def __init__(self, x, y, model, environment):
+        super().__init__(x, y, model, environment)
+        self.trajectoire = [[0.0625,0.0625],[0.0625,1-0.0625],[1-0.0625,1-0.0625],[1-0.0625,0.0625]]
+        self.step_traj = 0
+
+    def step(self):
+        self.goto(self.trajectoire[self.step_traj][0],self.trajectoire[self.step_traj][1])
+        if self.x == self.trajectoire[self.step_traj][0] and self.y == self.trajectoire[self.step_traj][1] :
+            self.step_traj += 1
+            if self.step_traj > 3:
+                self.step_traj = 0
+        
 
 
 if __name__ == "__main__":
-    team = [drone_reco, Dragster]
+    team = [Drone_reco, Antenne]
     run_single_server(team)
