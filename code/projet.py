5674612f38b41181572b4ae0760f96a39bace8eb
FEBZ <kevin.gazeau@isen-ouest.yncrea.fr>
2024-03-18 10:44:36 +0100

    final v2

--- code/projet.py
+++ code/projet.py
@@ -147,27 +147,22 @@ class Dragster2(Speeder):
                 self.person = elt
 
         if self.etape == "setup":
-            self.goto(0.51,0.51)
-            if self.x == 0.51 and self.y == 0.51 :
-                self.etape = "info"
+            if self.x != 0.51 and self.y != 0.51 :
+                self.goto(0.51,0.51)
 
-        if self.etape == "info":
-            messages = self.receive()
-            if len(messages) != 0 :
-                for msg in messages :
-                    if isinstance(msg.body,Person) :
-                        self.person = msg.body
-                    if isinstance(msg.body,RescueItem) :
-                            if len(self.items) == 0 :
-                                self.items.append(msg.body)
-                            else :
-                                for elt in self.items :    
-                                    if msg.body != elt :
-                                        self.items.append(msg.body)
-            if len(self.items) > self.items_rescued and self.person != "" :
-                self.etape = "rescue"
-            elif len(self.items) > self.items_brought and self.person == "" :
-                self.etape = "bring"
+        messages = self.receive()
+        if len(messages) != 0 :
+            for msg in messages :
+                if isinstance(msg.body,Person) :
+                    self.person = msg.body
+                if isinstance(msg.body,RescueItem) :
+                    if msg.body not in self.items :
+                        self.items.append(msg.body)
+                    
+        if len(self.items) > self.items_rescued and self.person != "" :
+            self.etape = "rescue"
+        elif len(self.items) > self.items_brought and self.person == "" :
+            self.etape = "bring"
 
         if self.etape == "rescue" :
             if self.taken_item == None and self.x != self.items[self.items_rescued].x and self.y != self.items[self.items_rescued].y :
@@ -179,8 +174,11 @@ class Dragster2(Speeder):
             elif self.taken_item != None and self.x == self.person.x and self.y == self.person.y :
                 self.drop_item()
                 self.items_rescued += 1
-                if len(self.items) == self.items_rescued :
+                if len(self.items) >= self.items_rescued :
                     self.etape = "setup"
+                    if len(self.items)>1:
+                        if self.items_rescued == 1 and (self.items[self.items_rescued].x == self.person.x and self.items[self.items_rescued].y == self.person.y) :
+                            self.items_rescued = 0
 
         if self.etape == "bring":
             if self.taken_item == None and self.x != self.items[self.items_brought].x and self.y != self.items[self.items_brought].y :
@@ -188,12 +186,12 @@ class Dragster2(Speeder):
             elif self.taken_item == None and self.x == self.items[self.items_brought].x and self.y == self.items[self.items_brought].y :
                 self.take(self.items[self.items_brought])
             elif self.taken_item != None and self.x != 0.51 and self.y != 0.51 :
-                self.goto(0.51,0.51)
+                self.goto(0.51,0.51+0.03*self.items_brought)
             elif self.taken_item != None and self.x == 0.51 and self.y == 0.51 :
                 self.drop_item()
                 self.items_brought += 1
                 if len(self.items) == self.items_brought :
-                    self.etape = "info"
+                    self.etape = "setup"
 
         """self.goto(self.destination[0],self.destination[1])
         if self.x == self.destination[0] and self.y == self.destination[1] :
@@ -214,37 +212,40 @@ class Antenne2(Balloon):
             if elt not in self.list_obj:
                 self.list_obj.append(elt)
 
+        messages = self.receive()
+        if len(messages) != 0 :
+            for msg in messages :
+                if msg.body not in self.list_obj :
+                    self.list_obj.append(msg.body)
+
         if self.etape == "reco" :
             self.goto(self.trajectoire[self.step_traj][0],self.trajectoire[self.step_traj][1])
             if self.x == self.trajectoire[self.step_traj][0] and self.y == self.trajectoire[self.step_traj][1] :
                 self.step_traj += 1
                 if self.step_traj % 2 == 0 :
-                    self.etape = "pre_comm"
+                    self.etape = "comm"
 
 
-        if self.etape == "pre_comm" :
+        if self.etape == "comm" :
             list_rob = self.model.retrieve_robots()
             for elt in self.list_obj:
-                i=1
                 for robot in list_rob:
                     if isinstance(robot,Dragster2):
                         self.list_msg.append(Message(robot,self,elt,"informer","position item"))
-                        print(f"msg ballon {i}")
-                        i += 1
-            self.etape = "comm"
-
-        if self.etape == "comm" :
-            for message in self.list_msg :
-                self.send(message)
-                print("message ballon sent")
+                    if isinstance(robot,Drone_reco2):
+                        self.list_msg.append(Message(robot,self,elt,"informer","position item"))
             if self.step_traj < 10:
                 self.etape = "reco"
+
+        if self.list_msg != [] :
+            for message in self.list_msg :
+                self.send(message)
 
 class Drone_reco2(Climber) :
     def __init__(self, x, y, model, environment):
         super().__init__(x, y, model, environment)
         self.etape = "reco"
-        self.trajectoire = [[0.0750*2,0.0750*2],[0.0625,0.0625],[0.0625,1-0.0625],[0.0750*2,1-0.0750*2],[0.0625,1-0.0625],[1-0.0625,1-0.0625],[1-0.0750*2,1-0.0750*2],[1-0.0625,1-0.0625],[1-0.0625,0.0625],[1-0.0750*2,0.0750*2],[1-0.0625,0.0625],[0.0625,0.0625]]
+        self.trajectoire = [[0.0625,0.5],[0.0625,1-0.0625],[0.0750*2,1-0.0750*2],[0.0625,1-0.0625],[0.5,1-0.0625],[1-0.0625,1-0.0625],[1-0.0750*2,1-0.0750*2],[1-0.0625,1-0.0625],[1-0.0625,0.5],[1-0.0625,0.0625],[1-0.0750*2,0.0750*2],[1-0.0625,0.0625],[0.5,0.0625],[0.0625,0.0625],[0.0750*2,0.0750*2],[0.0625,0.0625],[0.0625,0.5]]
         self.step_traj = 0
         self.list_msg = []
         self.list_obj = []
@@ -257,39 +258,39 @@ class Drone_reco2(Climber) :
         for elt in liste :
             if elt not in self.list_obj:
                 self.list_obj.append(elt)
+
+        messages = self.receive()
+        if len(messages) != 0 :
+            for msg in messages :
+                if msg.body not in self.list_obj :
+                    self.list_obj.append(msg.body)
+        
         if self.etape == "reco":
             self.goto(self.trajectoire[self.step_traj][0],self.trajectoire[self.step_traj][1])
             if self.x == self.trajectoire[self.step_traj][0] and self.y == self.trajectoire[self.step_traj][1] : 
                 self.step_traj += 1
-                if self.step_traj > 11 or len(self.list_obj) != self.nbs_obj :
+                if self.step_traj > 16 or (len(self.list_obj) != self.nbs_obj and (self.step_traj-1) % 4 == 0) :
                     if len(self.list_obj) != self.nbs_obj :
                         self.step_traj -= 1
                     self.nbs_obj = len(self.list_obj)
-                    self.etape = "traj_comm"
+                    self.etape = "comm"
         
-        elif self.etape == "traj_comm" :
+        elif self.etape == "comm" :
             self.goto(0.5,0.5)
             if self.x == 0.5 and self.y == 0.5 :
                 list_rob = self.model.retrieve_robots()
                 for elt in self.list_obj:
-                    i = 1
                     for robot in list_rob:
                         if isinstance(robot,Dragster2):
                             self.list_msg.append(Message(robot,self,elt,"informer","position item"))
-                            print(f"msg Climber {i}")
-                            i += 1
-                self.etape = "comm"
+                        if isinstance(robot,Antenne2):
+                            self.list_msg.append(Message(robot,self,elt,"informer","position item"))
+                if self.step_traj < 16:
+                    self.etape = "reco"
 
-        elif self.etape == "comm" :
+        if self.list_msg != [] :
             for message in self.list_msg :
                 self.send(message)
-                print("message climber sent")
-            if self.step_traj <= 11 :
-                if self.waiting_time < 10 :
-                    self.waiting_time +=1
-                else :
-                    self.waiting_time = 0
-                    self.etape = "reco"
         
 
 
